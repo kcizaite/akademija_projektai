ebe00f2a2f1f3807f730fe8b980b5bd8609f791d
Karolina Čižaitė <61018680+kcizaite@users.noreply.github.com>
2022-11-14 19:54:41 +0200

    Initial commit

---
+++ junitProject/src/test/java/project/carNumberCalc/CarNumberPricingCalculatorTest.java
@@ -0,0 +1,20 @@
+package project.carNumberCalc;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CarNumberPricingCalculatorTest {
+
+    @Test
+    void testCalculatePrice(){
+        CarNumberPricingCalculator cnpc = new CarNumberPricingCalculator();
+
+    }
+}
+
+
+//         int expected = 4;
+//        int result = cnpc.sum(2, 2);
+//        assertEquals(expected, result, "2 + 2 should be...");
+//        assertEquals(4, cnpc.sum(2, 2));
